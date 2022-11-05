e105803a9c0dc458c1868b77e6c022a41321e18e
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-05 13:00:12 +0200

    Add files via upload

---
+++ Students.cs
@@ -0,0 +1,54 @@
+﻿
+
+namespace _4._Students
+{
+    using System;
+    using System.Numerics;
+    using System.Collections.Generic;
+    using System.Linq;
+    class Students
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < n; i++)
+            {
+                List<string> studentArray = Console.ReadLine()
+                    .Split()
+                    .ToList();
+
+                string firstName = studentArray[0];
+                string lastName = studentArray[1];
+                decimal grade = decimal.Parse(studentArray[2]);
+
+                Student student = new Student(firstName, lastName, grade);
+
+                students.Add(student);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, students.OrderByDescending(student => student.Grade)));
+        }
+    }
+
+    public class Student
+    {
+        public Student(string firstName, string lastName, decimal grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade}";
+        }
+    }
+}
